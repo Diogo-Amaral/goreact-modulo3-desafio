01f91fff674d4446107a5c43c84f97373d124ff1
Diogo Amaral <diogo-amaral@live.com>
2019-04-23 17:58:49 -0300

    desafio3

--- src/components/ControlPanel.js
+++ src/components/ControlPanel.js
@@ -1,11 +1,21 @@
 import React from "react";
 
-import { Container } from "./styles";
+import { Container, Developer } from "./styles";
 
 const ControlPanel = () => {
   return (
     <Container>
       <p>Marcações</p>
+      <Developer>
+        <li>
+          <img
+            src="https://avatars3.githubusercontent.com/u/14218370?v=4"
+            alt="avatar"
+          />
+          <strong>Diogo Amaral</strong>
+          <small>Diogo-Amaral</small>
+        </li>
+      </Developer>
     </Container>
   );
 };
--- src/components/styles.js
+++ src/components/styles.js
@@ -2,9 +2,11 @@ import styled from "styled-components";
 
 export const Container = styled.div`
   position: absolute;
+  height: 80%;
+  width: 200px;
   top: 0;
   left: 0;
-  max-width: 320px;
+  max-width: 400px;
   background: #fff;
   box-shadow: 0 2px 4px rgba(0, 0, 0, 0.3);
   padding: 12px 24px;
@@ -12,10 +14,28 @@ export const Container = styled.div`
   font-size: 13px;
   line-height: 2;
   color: #6b6b76;
-  text-transform: uppercase;
   outline: none;
 
   p {
     font-size: 16px;
+  }
+`;
+
+export const Developer = styled.ul`
+  li {
+    display: flex;
+    flex-direction: column;
+    img {
+      width: 50px;
+      height: 50px;
+    }
+
+    strong {
+      font-size: 14px;
+    }
+
+    small {
+      font-size: 10px;
+    }
   }
 `;
